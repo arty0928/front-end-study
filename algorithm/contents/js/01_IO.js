c62be1d32198aa9e0e2e1144b10017e14f8bb737
M4024\student <arty0928@konkuk.ac.kr>
2025-02-07 13:10:22 +0900

    [실습] 알고리즘_JS  IOTest

---
+++ algorithm/contents/js/01_IO.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const filePath = process.platform == 'linux'? '/dev/stdin':__dirname + '\\input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const tc = Number(input[0]);
+const testcases = [];
+for(let i = 1; i <= tc; i++){
+
+  // i번째 라인의 데이터를 공백을 기분으로 분리하고 
+  // .map(Number)를 이용해서 바로 숫자로 변경
+  const temp = input[i].split(" ").map(Number)
+  const testcase = {N : temp[0], data: temp.slice(1)}
+  testcases.push(testcase)
+}
+console.log(testcases)
---
+++ algorithm/week1/2438_별찍기-1.js
@@ -0,0 +1,4 @@
+var input = require('fs').readFileSync('/dev/stdin').toString();
+var num = parseInt(input);
+
+console.log(num);
