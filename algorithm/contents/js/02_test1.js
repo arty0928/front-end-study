d1f19b52ede0405f3e8e016ac04d9873c97c67a1
M4024\student <arty0928@konkuk.ac.kr>
2025-02-11 09:05:02 +0900

    [실습] 클래스, 객체, 생성자

---
+++ algorithm/contents/js/02_test1.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux'?"/dev/stdin":__dirname+"/input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const inputCount = Number(input[0]);
+const testCases = [];
+
+for (let i = 1; i <= inputCount; i++) {
+  const temp = input[i].split(" ").map(Number);
+  const testCase = {
+    N: temp[0],
+    scores: temp.slice(1),
+  };
+
+  testCases.push(testCase);
+}
+
+console.log(testCases);
+
+const solution = {
+  ({N, scores}) => {
+    
+  }
+}
